d4adf94f9a6ef4f3232be7d4424972383f3c61f9
Blake Womack <lakebayomackway@gmail.com>
2017-03-16 14:26:41 -0700

    add finished project as example.

--- src/main/java/LeapYear.java
+++ src/main/java/LeapYear.java
@@ -1,8 +1,12 @@
 public class LeapYear {
 
   public boolean isLeapYear(int year) {
-    // eventually put your code here
-    return false;
+    if (year % 400 == 0) {
+      return true;
+    } else if (year % 100 == 0) {
+      return false;
+    } else {
+      return year % 4 == 0;
+    }
   }
-
 }
